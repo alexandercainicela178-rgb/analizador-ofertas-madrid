c44e22aa904f85464d292306312deed51187e65e
alexandercainicela178-rgb <alexandercainicela178@gmail.com>
2026-09-20 16:23:43 -0500

    Crear proyecto _final_madrid.py

---
+++ proyecto _final_madrid.py
@@ -0,0 +1,21 @@
+# 🇪🇸 Analizador de Ofertas - Madrid
+**De Cajamarca a Madrid con Python**
+
+Mi primera IA que calcula si una oferta de trabajo en España te permite vivir solo.
+
+### ¿Qué hace?
+Convierte sueldo bruto anual (ej: 52.000€) a neto mensual en 14 pagas y resta gastos reales de Madrid:
+- 1150€ Alquiler
+- 400€ Comida
+- 150€ Transporte/Otros
+= Te dice cuanto te queda LIBRE
+
+### Resultados reales
+- 28.000€/año -> -180€ ❌ No alcanza ni para vivir solo
+- 45.000€/año Telefónica -> +743€ 👍 Justo
+- 52.000€/año Cabify -> +1.123€ ✅ Excelente, ahorras
+
+### Tecnología
+Python - Funciones, condicionales, cálculos
+
+Creado en 5 días desde 0 - Bootcamp Python para IA
